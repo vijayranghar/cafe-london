60adf9580ba21ba68920cf539d2cfb052dd05b09
VijayRanghar <vijayranghar@gmail.com>
2018-06-19 23:57:13 +0530

    collaboratewithus style tweaks

--- src/components/CollaborateWithUs/index.js
+++ src/components/CollaborateWithUs/index.js
@@ -7,25 +7,35 @@ class CollaborateWithUs extends Component {
     return (
       <section className="collaborate-with-us">
         <div className="container">
-          <h2>Collaborate with us</h2>
+          <div className="collaborate-with-us--info">
+            <h2>Collaborate <strong>with us</strong></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+            in voluptate velit esse cillum dolore
+            </p>
+          </div>
           <form className="collaborate-with-us--form">
-            <div>
+            <div className="collaborate-with-us--form--group">
               <label>Name</label>
               <input type="text" placeholder="Name" />
             </div>
-            <div>
+            <div className="collaborate-with-us--form--group">
               <label>Surname</label>
               <input type="text" placeholder="Surname" />
             </div>
-            <div>
+            <div className="collaborate-with-us--form--group">
               <label>Phone</label>
               <input type="tel" placeholder="Phone" />
             </div>
-            <div>
+            <div className="collaborate-with-us--form--group">
               <label>Email address</label>
               <input type="email" placeholder="Email" />
             </div>
-            <div>
+            <div className="collaborate-with-us--form--group">
               <label>Experience</label>
               <select placeholder="Select">
                 <option value="1">1</option>
@@ -35,7 +45,7 @@ class CollaborateWithUs extends Component {
                 <option value="5">5+</option>
               </select>
             </div>
-            <div>
+            <div className="collaborate-with-us--form--group">
               <label>Where did you hear about us?</label>
               <select placeholder="Select">
                 <option value="Magazine">Magazine</option>
@@ -44,6 +54,7 @@ class CollaborateWithUs extends Component {
                 <option value="Website">Website</option>
               </select>
             </div>
+            <button type="submit">Send</button>
           </form>
         </div>
       </section>
