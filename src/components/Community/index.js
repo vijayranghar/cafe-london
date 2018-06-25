49b9eae10ff2f01c25e678ca7fab798b7a409a99
VijayRanghar <vijayranghar@gmail.com>
2018-06-25 10:10:14 +0530

    some more ui t more UI tweaks

--- src/components/Community/index.js
+++ src/components/Community/index.js
@@ -7,7 +7,8 @@ class Community extends Component {
     return (
       <section className="community">
         <div className="container">
-          <h2>Meet our community</h2>
+          <h2>Meet our</h2>
+          <h2 className="heading-strikethrough">community</h2>
           <div className="badge">Know more</div>
           <p>LOREM IPSUM DOLOR SIT AMET ERNUT TEMPARTERO SERTU PER NABORE EN TORNA ENTALTO</p>
         </div>
