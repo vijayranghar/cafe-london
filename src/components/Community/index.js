98130fd9ea333c45de6233590157c74c06f630b9
VijayRanghar <vijayranghar@gmail.com>
2018-08-18 22:19:17 +0530

    community fixes

--- src/components/Community/index.js
+++ src/components/Community/index.js
@@ -9,16 +9,22 @@ class Community extends Component {
         <div className="container">
           <div className="column">
             <div className="community__tweet">
-              <span>@love_food</span>
-              <p>
-                Waffle sticks in Copenhagen! <span role='img' aria-label="smiley">🇩🇰😍</span>
-                Milk chocolate with sprinkles, dark chocolate
-                with coconut and milk chocolate with peanuts!
-              <span role='img' aria-label="camera">[📷:</span> @foodwithmichel] #lovefood
-                <FontAwesome name="rocket" />
-              </p>
+              <div className="community__tweet-details">
+                <span>@love_food</span>
+                <p>
+                  Waffle sticks in Copenhagen! <span role='img' aria-label="smiley">🇩🇰😍</span>
+                  Milk chocolate with sprinkles, dark chocolate
+                  with coconut and milk chocolate with peanuts!
+                <span role='img' aria-label="camera">[📷:</span> @foodwithmichel] #lovefood
+                  <FontAwesome name="rocket" />
+                </p>
+              </div>
+              <div className="community__tweet-count">
+                <FontAwesome name="comment" />&nbsp;&nbsp;212
+                <FontAwesome name="heart" />&nbsp;&nbsp;14.1k
+              </div>
             </div>
-            <div>
+            <div className="community__recipe-info">
               <p>
                 Lorem ipsum dolor sit amet,
                 consectetur adipiscing elit, sed do eiusmod tempor
@@ -35,14 +41,14 @@ class Community extends Component {
               <p>LOREM IPSUM DOLOR SIT AMET ERNUT TEMPARTERO SERTU PER NABORE EN TORNA ENTALTO</p>
             </div>
             <div className="community__recipe-image">
-              <img className="img-responsive" src={require("../../assets/images/burger.jpg")} alt="burger" />
+              <div></div>
             </div>
           </div>
           <div className="column">
             <div className="community__recipe-image">
-              <img className="img-responsive"  src={require("../../assets/images/pizza.jpg")} alt="pizza" />
+              <div></div>
             </div>
-            <div>
+            <div className="community__recipe-info">
               <p>
                 Ut enim ad minim veniam, quis nostrud
                 exercitation ullamco laboris nisi ut aliquip ex ea
@@ -51,15 +57,21 @@ class Community extends Component {
               </p>
             </div>
             <div className="community__tweet">
-              <span>@buzzfeedfood</span>
-              <p>
-                Getting into long weekend mode like <span role='img' aria-label="food">🍤 </span>#pancitpalabok (<span role='img' aria-label="camera">📷 </span>@jeepneynyc)
-              </p>
+              <div className="community__tweet-details">
+                <span>@buzzfeedfood</span>
+                <p>
+                  Getting into long weekend mode like <span role='img' aria-label="food">🍤 </span>#pancitpalabok (<span role='img' aria-label="camera">📷 </span>@jeepneynyc)
+                </p>
+              </div>
+              <div className="community__tweet-count">
+                <FontAwesome name="comment" />&nbsp;&nbsp;120
+                <FontAwesome name="heart" />&nbsp;&nbsp;12.1k
+              </div>
             </div>
           </div>
           <div className="column">
             <div className="community__tweet">
-              <div>
+              <div className="community__tweet-details">
                 <span>@buzzfeedfood</span>
                 <p>
                   It’s taco Tuesday! These tacos from @jesseszewczyk
@@ -68,12 +80,13 @@ class Community extends Component {
                   @taylormillerphoto
                 </p>
               </div>
-              <div>
-
+              <div className="community__tweet-count">
+                <FontAwesome name="comment" />&nbsp;&nbsp;60
+                <FontAwesome name="heart" />&nbsp;&nbsp;4.1k
               </div>
             </div>
             <div className="community__recipe-image">
-              <img className="img-responsive" src={require("../../assets/images/cherries.jpg")} alt="cherries" />
+              <div></div>
             </div>
           </div>
         </div>
